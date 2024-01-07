38c8620188c05f644d1f529539be41f1bc9897ef
Marzieh <marzieh.tajik1996@gmail.com>
2024-01-07 11:45:43 +0330

    Added entities and migrations

---
+++ src/Entity/Car.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\CarRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\DBAL\Types\Types;
+
+#[ORM\Entity(repositoryClass: CarRepository::class)]
+#[ORM\Table(name: 'cars')]
+#[ApiResource]
+class Car
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: Types::INTEGER, nullable: false)]
+    private int $id;
+
+    #[ORM\Column(type: Types::STRING, nullable: true)]
+    private ?string $brand = null;
+
+    #[ORM\Column(type: Types::STRING, nullable: true)]
+    private ?string $model = null;
+
+    #[ORM\Column(type: Types::STRING, nullable: true)]
+    private ?string $color = null;
+}
---
+++ src/Entity/Review.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ReviewRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: ReviewRepository::class)]
+#[ORM\Table(name: 'reviews')]
+#[ApiResource]
+class Review
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: Types::INTEGER, nullable: false)]
+    private int $id;
+
+    #[ORM\Column(type: Types::INTEGER, nullable: true)]
+    #[Assert\Range(min: 1, max: 10, notInRangeMessage: 'Star rating must be between 1 and 10')]
+    private ?int $star_rating = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $review_text = null;
+}
