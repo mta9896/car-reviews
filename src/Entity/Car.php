543c4d4048ab37add5e6396dce75f4e2d3300e34
Marzieh <mta9896@gmail.com>
2024-01-09 09:05:29 +0330

    Edited entities and migrations

--- src/Entity/Car.php
+++ src/Entity/Car.php
@@ -5,29 +5,74 @@ namespace App\Entity;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\CarRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\DBAL\Types\Types;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CarRepository::class)]
-#[ORM\Table(name: 'cars')]
 #[ApiResource]
 class Car
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column(type: Types::INTEGER, nullable: false)]
+    #[ORM\Column]
     private int $id;
 
-    #[ORM\Column(type: Types::STRING, nullable: true)]
-    private ?string $brand = null;
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    private string $brand;
 
-    #[ORM\Column(type: Types::STRING, nullable: true)]
-    private ?string $model = null;
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    private string $model;
 
-    #[ORM\Column(type: Types::STRING, nullable: true)]
-    private ?string $color = null;
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    private string $color;
 
-    public function getId(): ?int{
-        
+    public function getId(): ?int
+    {
         return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(string $brand): static
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(string $model): static
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): static
+    {
+        $this->color = $color;
+
+        return $this;
     }
 }
--- src/Entity/Review.php
+++ src/Entity/Review.php
@@ -9,24 +9,55 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ReviewRepository::class)]
-#[ORM\Table(name: 'reviews')]
 #[ApiResource]
 class Review
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column(type: Types::INTEGER, nullable: false)]
+    #[ORM\Column]
     private int $id;
 
-    #[ORM\Column(type: Types::INTEGER, nullable: true)]
+    #[ORM\Column]
     #[Assert\Range(min: 1, max: 10, notInRangeMessage: 'Star rating must be between 1 and 10')]
-    private ?int $star_rating = null;
+    #[Assert\NotBlank]
+    private int $starrating;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
-    private ?string $review_text = null;
+    private ?string $reviewtext = null;
 
-    public function getId(): ?int{
-        
+    public function getId(): ?int
+    {
         return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getStarrating(): ?int
+    {
+        return $this->starrating;
+    }
+
+    public function setStarrating(int $starrating): static
+    {
+        $this->starrating = $starrating;
+
+        return $this;
+    }
+
+    public function getReviewtext(): ?string
+    {
+        return $this->reviewtext;
+    }
+
+    public function setReviewtext(?string $reviewtext): static
+    {
+        $this->reviewtext = $reviewtext;
+
+        return $this;
     }
 }
